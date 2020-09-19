92b1dc8ebd00592abb9bfebf6cec9915e64b818b
savan <vsavan7@gmail.com>
2020-09-18 19:14:41 -0500

    fix visualization issues

--- TestTimeAugmentation/visualize.py
+++ TestTimeAugmentation/visualize.py
@@ -2,6 +2,8 @@ import glob
 from generate_xml_cvat import get_labels_from_json
 import os
 import cv2
+import xml.etree.ElementTree as ET
+
 
 def visualize_images(pathImg):
     """
@@ -12,15 +14,17 @@ def visualize_images(pathImg):
     _images.extend(glob.glob("/mnt/data/dataorig/images/*.png"))
     for image in data['images']:
         tree = ET.parse(os.path.join(pathImg+"output", os.path.basename(image['file_name']))[:-4]+'.xml')
-        root = tree.getroot()    colors = {'zero':(0,255,0), 'light':(0,0,255),'medium':(255,0,0),'high':(120,120,0),'non_recoverable':(0,120,120),'cut':(0,0,0)}
-
+        root = tree.getroot()   
+        boxes = []
         for obj in root.findall('object'):
-            draw_boxes(os.path.basename(image['file_name']), obj.find('name').text,float(list(obj.iter('xmin'))[0].text),float(list(obj.iter('ymin'))[0].text),float(list(obj.iter('xmax'))[0].text),float(list(obj.iter('ymax'))[0].text))
+            boxes.append([obj.find('name').text,float(list(obj.iter('xmin'))[0].text),float(list(obj.iter('ymin'))[0].text),float(list(obj.iter('xmax'))[0].text),float(list(obj.iter('ymax'))[0].text)])
+        draw_boxes(os.path.basename(image['file_name']), boxes)
 
 
-def draw_boxes(img_path,label, xmin, ymin, xmax, ymax):
-    frame = cv2.imread("/mnt/data/dataorig/images"+img_path)
-    img = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-    cv2.rectangle(img, (xmin,ymin), (xmax,ymax), (255,0,0) , 5)
-    cv2.imwrite(os.path.join("/mnt/output", img_path), img)
-  
+def draw_boxes(img_path,boxes):
+
+    frame = cv2.imread("/mnt/data/dataorig/images/"+img_path,cv2.IMREAD_UNCHANGED)
+#     img = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+    for box in boxes:
+        cv2.rectangle(frame, (int(box[1]),int(box[2])), (int(box[3]),int(box[4])), (255,0,0) , 5)
+    cv2.imwrite(os.path.join("/mnt/output", img_path), frame)
