f8c87d75c76bc1e8af62853c8c12b688408456f6
inohmonton99 <inohfrancismonton@gmail.com>
2021-07-16 03:05:21 +0800

    GraphDef() compatibility

--- TestTimeAugmentation/predict_batch_tfod.py
+++ TestTimeAugmentation/predict_batch_tfod.py
@@ -99,7 +99,7 @@ def mainDataset(dataset,output, confidence,weights,fichClass):
     
     detection_graph = tf.Graph()
     with detection_graph.as_default():
-        od_graph_def = tf.GraphDef()
+        od_graph_def = tf.compat.v1.GraphDef()
         with tf.gfile.GFile(weights , 'rb') as fid:
             serialized_graph = fid.read()
             od_graph_def.ParseFromString(serialized_graph)
