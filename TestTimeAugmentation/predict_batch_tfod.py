4cee4b41089586479e8e7b6c589b19b71a2646e8
inohmonton99 <inohfrancismonton@gmail.com>
2021-07-16 03:22:57 +0800

    gfile compat to tf 2.3.x

--- TestTimeAugmentation/predict_batch_tfod.py
+++ TestTimeAugmentation/predict_batch_tfod.py
@@ -100,7 +100,7 @@ def mainDataset(dataset,output, confidence,weights,fichClass):
     detection_graph = tf.Graph()
     with detection_graph.as_default():
         od_graph_def = tf.compat.v1.GraphDef()
-        with tf.gfile.GFile(weights , 'rb') as fid:
+        with tf.io.gfile.GFile(weights , 'rb') as fid:
             serialized_graph = fid.read()
             od_graph_def.ParseFromString(serialized_graph)
             tf.import_graph_def(od_graph_def, name='')
